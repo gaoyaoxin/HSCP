1862152be688b89e3307b78dbd6191e5f32d17dc
高垚鑫 <gaoyaoxin@bfsu.edu.cn>
2018-03-22 08:58:36 +0800

    update

--- Chapter 18/readme.md
+++ Chapter 18/readme.md
@@ -1,3 +1,4 @@
 ||||
 -|-|-
 P195-196|18.4 单词模式模块的源代码|makeWordPatterns.py
+P202-205|18.12 简单替代破译程序的源代码|simpleSubHacker.py
---
+++ Chapter 18/simpleSubHacker.py
@@ -0,0 +1,47 @@
+﻿# Simple Substitution Cipher Hacker
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+import os, re, copy, pprint, pyperclip, simpleSubCipher, makeWordPatterns
+
+if not os.path.exists('wordPatterns.py'):
+	makeWordPatterns.main() # create the wordPatterns.py file
+import wordPatterns
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+nonLettersOrSpacePattern = re.compile('[^A-Z\s]')
+
+def main():
+	message = 'Sy l nlx sr pyyacao l ylwj eiswi upar lulsxrj isr sxrjsxwjr, ia esmm rwctjsxsza sj wmpramh, lxo txmarr jia aqsoaxwa sr pqaceiamnsxu, ia esmm caytra jp famsaqa sj. Sy, px jia pjiac ilxo, ia sr pyyacao rpnajisxu eiswi lyypcor l calrpx ypc lwjsxu sx lwwpcolxwa jp isr sxrjsxwjr, ia esmm lwwabj sj aqax px jia rmsuijarj aqsoaxwa. Jia pcsusx py nhjir sr agbmlsxao sx jisr elh. -Facjclxo Ctrramm'
+
+	# Determine the possible valid ciphertext translations.
+	print('Hacking...')
+	letterMapping = hackSimpleSub(message)
+
+	# Display the results to the user.
+	print('Mapping:')
+	pprint.pprint(letterMapping)
+	print()
+	print('Original ciphertext:')
+	print(message)
+	print()
+	print('Copying hacked message to clipboard:')
+	hackedMessage = decryptWithCipherletterMapping(message, letterMapping)
+	pyperclip.copy(hackedMessage)
+	print(hackedMessage)
+
+
+def getBlankCipherletterMapping():
+	# Returns a dictionary value that is a blank cipherletter mapping.
+	return {'A': [], 'B': [], 'C': [], 'D': [], 'E': [], 'F': [], 'G': [], 'H': [], 'I': [], 'J': [], 'K': [], 'L': [], 'M': [], 'N': [], 'O': [], 'P': [], 'Q': [], 'R': [], 'S': [], 'T': [], 'U': [], 'V': [], 'W': [], 'X': [], 'Y': [], 'Z': [], }
+
+
+def addLettersToMapping(letterMapping, cipherword, candidate):
+	# The letterMapping parameter is a "Cipherletter mapping" dictionary
+	# value that the return value of this function starts as a copy of.
+	# The cipherword parameter is a string value of the ciphertext word.
+	# The candidate parameter is a possible English word that the
+	# cipherword could decrypt to.
+	
+	# This function adds the letters of the candidate as potential
+	# decryption letters for the cipherletters in the cipherletter
+	# mapping.
